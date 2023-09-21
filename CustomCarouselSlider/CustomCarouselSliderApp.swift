76c5f19b957c3bae23f7e1e742744bf82cbe8dc3
Yunus Emre Taşdemir <yetsdmr@gmail.com>
2023-09-21 19:45:53 +0300

    Initial Commit

---
+++ CustomCarouselSlider/CustomCarouselSliderApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomCarouselSliderApp.swift
+//  CustomCarouselSlider
+//
+//  Created by Yunus Emre Taşdemir on 21.09.2023.
+//
+
+import SwiftUI
+
+@main
+struct CustomCarouselSliderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
